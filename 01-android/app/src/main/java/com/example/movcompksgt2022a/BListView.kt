cb3bb9bca1d3e82ab396a348102cb78c47f79730
Kevin Guachi <kevin.guachi@epn.edu.ec>
2022-06-17 12:14:41 -0500

    Hasta añadir número
    Método añadir

--- 01-android/app/src/main/java/com/example/movcompksgt2022a/BListView.kt
+++ 01-android/app/src/main/java/com/example/movcompksgt2022a/BListView.kt
@@ -3,13 +3,15 @@ package com.example.movcompksgt2022a
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ListView
 
 class BListView : AppCompatActivity() {
+    val arreglo: ArrayList<Int> = arrayListOf(1, 2, 3,4,5)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blist_view)
-        val arreglo: ArrayList<Int> = arrayListOf(1, 2, 3)
         val listView = findViewById<ListView>(R.id.lv_list_view)
         val adaptador = ArrayAdapter(
             this,//contexto
@@ -17,6 +19,20 @@ class BListView : AppCompatActivity() {
             arreglo
         )
         listView.adapter = adaptador
+        adaptador.notifyDataSetChanged()
+
+        val botonAniadirLv = findViewById<Button>(R.id.btn_anadir_list_view)
+
+        botonAniadirLv
+            .setOnClickListener{
+                aniadirNumero(adaptador)
+            }
+    }
+    
+    fun aniadirNumero(
+        adaptador:ArrayAdapter<Int>
+    ){
+        arreglo.add(1)
         adaptador.notifyDataSetChanged()
     }
 }
