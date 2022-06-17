920dd2f89135f96fec6323444a8830c50b899265
Kevin Guachi <kevin.guachi@epn.edu.ec>
2022-06-17 12:01:53 -0500

    Notify

--- 01-android/app/src/main/java/com/example/movcompksgt2022a/BListView.kt
+++ 01-android/app/src/main/java/com/example/movcompksgt2022a/BListView.kt
@@ -2,10 +2,21 @@ package com.example.movcompksgt2022a
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class BListView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blist_view)
+        val arreglo: ArrayList<Int> = arrayListOf(1, 2, 3)
+        val listView = findViewById<ListView>(R.id.lv_list_view)
+        val adaptador = ArrayAdapter(
+            this,//contexto
+            android.R.layout.simple_list_item_1, //como se va a ver (xml)
+            arreglo
+        )
+        listView.adapter = adaptador
+        adaptador.notifyDataSetChanged()
     }
 }
